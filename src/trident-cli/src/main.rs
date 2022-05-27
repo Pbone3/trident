90f6b52f696dc71d7e14e25ade6be8900c590653
Pbone3 <pbone785@gmail.com>
2022-05-27 00:13:36 -0500

    CLI crate setup and gitignore fix

--- src/trident-cli/src/main.rs
+++ src/trident-cli/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let question = requestty::Question::select("theme")
+    .message("What do you want to do?")
+    .choices(vec![
+        "Foobar".into(),
+        "Xyzzy".into(),
+        "Plugh".into(),
+        requestty::DefaultSeparator,
+        "Cancel".into()
+    ])
+    .build();
+
+println!("{:#?}", requestty::prompt_one(question));
 }
